0dbc254dc7619fd810b3e55661fabbb7491f8fc1
Swati singh <swatigola01@gmail.com>
2025-09-27 17:36:07 +0530

    add redux toolkit

--- json/src/components/Counter.jsx
+++ json/src/components/Counter.jsx
@@ -1,41 +1,45 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import "./Counter.css"
+import { counterAction } from "../store";
 const Counter = () => {
   let count = useSelector(state => state.count);
     let dispatch = useDispatch();
-
+let showCounter = useSelector(state=>state.showCounter)
   const handleIncrement = () => {
-    dispatch({type:"inc"})
+    dispatch(counterAction.increment())
   };
 
   const handleDecrement = () => {
-   dispatch({type:"dec"})
+   dispatch(counterAction.decrement())
     };
     const handleIncrementBy2 = () => {
-   dispatch({type:"incrementBy2"})
+   dispatch(counterAction.incermentBy2())
     }
     const handleDecrementBy2 = () => {
-   dispatch({type:"decrementBy2"})
+   dispatch(counterAction.decrementBY2())
     };
-    const handleDecrementBy5 = () => {
-   dispatch({type:"decrementBy5"})
-    };
-    const handleIncrementBy5 = () => {
-   dispatch({type:"incrementBy5"})
-  };
+//     const handleDecrementBy5 = () => {
+//    dispatch({type:"decrementBy5"})
+//     };
+//     const handleIncrementBy5 = () => {
+//    dispatch({type:"incrementBy5"})
+//     };
+   const handleToggle = () => {
+    dispatch(counterAction.toggle())
+    }
 
   return (
     <div className='container'>
       <h1>Redux Counter</h1>
-      <div className='value'>{count}</div>
-      <button onClick={handleIncrement} className='btn'>Increment</button>
-          <button onClick={handleDecrement} className='btn'>Decrement</button>
-          
-           <button onClick={handleIncrementBy2} className='btn'>Increment By 2</button>
-          <button onClick={handleDecrementBy2} className='btn'>Decrement By 2</button>
-          <button onClick={handleIncrementBy5} className='btn'>Increment By 5</button>
-      <button onClick={handleDecrementBy5} className='btn'>Decrement By 5</button>
+     {showCounter&&<div className='value'>{count}</div>}
+             <button onClick={handleIncrement} className='btn'>Increment</button>
+             <button onClick={handleDecrement} className='btn'>Decrement</button>
+             <button onClick={handleIncrementBy2} className='btn'>Increment By 2</button>
+             <button onClick={handleDecrementBy2} className='btn'>Decrement By 2</button>
+             {/* <button onClick={handleIncrementBy5} className='btn'>Increment By 5</button>
+             <button onClick={handleDecrementBy5} className='btn'>Decrement By 5</button> */}
+            <button onClick={handleToggle}>Toggle Counter</button>
     </div>
   );
 };
--- json/src/store/index.js
+++ json/src/store/index.js
@@ -1,64 +1,41 @@
-// const redux = require("redux")
 
 
-// const counterReducer = (state = { count:0 }, { type }) => {
-//     if (type == "inc") {
-//         return {
-//             count: state.count + 1,
-//         }
-//     } else if(type == "dec") {
-//         return {
-//         count :state.count-1,
-//         }
-//     }
-    
-//     return state
-
-// }
-// const store = redux.legacy_createStore(counterReducer)
-// console.log(store.getState())
-
-// const countersubscribe = () => {
-//     const latestState = store.getState()
-//     console.log(latestState)
-// }
-// store.subscribe(countersubscribe)
-// store.dispatch({ type: "inc" })
-// store.dispatch({type:"dec"})
 
 
-import { legacy_createStore } from "redux"
-const counterReducer = (state={count:0},action) => {
-    if (action.type == "inc") {
-        return {
-            count: state.count + 1,
-            
-        }
-    } else if (action.type == "dec") {
-        return {
-            count: state.count - 1,
-        }
-    }else if (action.type == "incrementBy2") {
-        return {
-            count: state.count + 2,
-        }
-    }else if (action.type == "decrementBy2") {
-        return {
-            count: state.count - 2,
-        }
-    }else if (action.type == "incrementBy5") {
-        return {
-            count: state.count + 5,
-        }
-    }else if (action.type == "decrementBy5") {
-        return {
-            count: state.count - 5,
-        }
-    }
-    else {
-        return state;
-    }
+import { createSlice ,configureStore} from "@reduxjs/toolkit"
+
+let initialState = {
+    count: 0,
+    showCounter:false,
 }
+const counterSlice=createSlice({
+    name: "counter",
+    initialState,
+    reducers: {
+        increment(state) {
+            state.count++;
+        },
+        decrement(state) {
+            state.count--;
+        },
+        incermentBy2(state) {
+         state.count=state.count+2;
+        },
+        decrementBY2(state) {
+        state.count=state.count-2;
+        },
+        toggle(state) {
+            state.showCounter = !state.showCounter;
+        },
+        
+    }
+})
 
-const store = legacy_createStore(counterReducer)
+
+const store = configureStore({
+    reducer: counterSlice.reducer,
+    
+    
+})
+export const counterAction = counterSlice.actions
 export default store
